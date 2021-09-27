808d46b201111961fc8103be25b79f18470b20f5
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2021-09-27 14:43:28 +0100

    Initial test setup

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 import Drag from "./expand.js"
+
+const containerElement = document.querySelector("#container-element")
+const handler = document.querySelector("#handler")
+
+new Drag(containerElement, handler).listen()
