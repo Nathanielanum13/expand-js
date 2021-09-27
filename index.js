ede02da906a30857afad3577fbd923c1e6fa67b2
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2021-09-27 16:40:23 +0100

    Removed test files

--- index.js
+++
@@ -1,6 +0,0 @@
-import Drag from "./expand.js"
-
-const containerElement = document.querySelector("#container-element")
-const handler = document.querySelector("#handler")
-
-new Drag(containerElement, handler).listen()
